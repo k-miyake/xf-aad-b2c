279bb5425cfd503aee872d99be598503100cfaba
Kazuyuki Miyake <miyake@zenarc.jp>
2016-12-07 23:51:43 +0900

    AndroidへのAAD認証の実装を追加

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -8,6 +8,9 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
+using Microsoft.WindowsAzure.MobileServices;
+using System.Threading.Tasks;
+
 namespace xfb2capp.Droid
 {
 	[Activity (Label = "xfb2capp.Droid",
@@ -15,9 +18,12 @@ namespace xfb2capp.Droid
 		MainLauncher = true,
 		ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
 		Theme = "@android:style/Theme.Holo.Light")]
-	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
-	{
-		protected override void OnCreate (Bundle bundle)
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity, IAuthenticate
+    {
+        // 認証ユーザ
+        private MobileServiceUser user;
+
+        protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 
@@ -27,9 +33,41 @@ namespace xfb2capp.Droid
 			// Initialize Xamarin Forms
 			global::Xamarin.Forms.Forms.Init (this, bundle);
 
-			// Load the main application
-			LoadApplication (new App ());
-		}
-	}
-}
+            // Authenticatorの初期化
+            App.Init((IAuthenticate)this);
 
+            // Load the main application
+            LoadApplication (new App ());
+		}
+
+        // Azure AD B2C認証
+        public async Task<bool> Authenticate()
+        {
+            var success = false;
+            var message = string.Empty;
+            try
+            {
+                user = await TodoItemManager.DefaultManager.CurrentClient.LoginAsync(this,
+                    MobileServiceAuthenticationProvider.WindowsAzureActiveDirectory);
+                if (user != null)
+                {
+                    message = string.Format("{0}として認証されています",
+                        user.UserId);
+                    success = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+            }
+
+            // 認証後のメッセージ
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.SetMessage(message);
+            builder.SetTitle("サインイン状況");
+            builder.Create().Show();
+
+            return success;
+        }
+    }
+}
