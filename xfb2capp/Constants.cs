0296b7f35e502686a2adadf4df82031f889990e8
Kazuyuki Miyake <miyake@zenarc.jp>
2016-12-07 22:43:05 +0900

    initial commit

---
+++ xfb2capp/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace xfb2capp
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://xfb2capp.azurewebsites.net";
+	}
+}
+
